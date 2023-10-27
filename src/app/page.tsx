a675838cbdef60ec86dd4364483daf7b141448b8
Vlad Dobrij <vlad.dobrij123@gmail.com>
2023-10-27 20:20:11 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,50 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+import * as React from "react";
+import Droppable from "./Components/Layout/utils/Droppable";
+import SideBar from "./Components/Layout/SideBar";
+import CurvesCanvas from "./Components/BezierCurves/CurvesCanvas";
+import Drawing from "./Components/Drawing/Drawing";
+import Selection from "./Components/Selection/Selection";
+import TextTool from "./Components/TextObject/TextTool";
+import RenderResizeShape from "./Components/shape/RenderResizeShape";
+import RemoveObject from "./Components/Layout/utils/RemoveObject";
+import {useHome} from "./useHome";
+
+function Home() {
+
+
+    const {
+        option,
+        setOption,
+        setShape,
+        handleRemoveShape,
+        handleAddShape,
+        shapes,
+    } = useHome()
+
+    return (
+
+        <div className={'back'} onClick={handleAddShape}>
+
+            <div className={'background'}/>
+            <Droppable>
+                <>
+                    {shapes.map(shape => {
+                        return <RemoveObject key={shape.id} handleRemove={handleRemoveShape} id={shape.id}>
+                            <RenderResizeShape item={shape} />
+                        </RemoveObject>
+                    })}
+                    <Selection isUsed={option === "Selection"}/>
+                    <Drawing isUsed={option === "Drawing"} isUsable={option}/>
+                    <CurvesCanvas add={option === "Curve"} setShape={setOption} isUsable={option}/>
+                    <TextTool isUsed={option === "Text"} setOption={setOption}/>
+                </>
+            </Droppable>
+            <SideBar setShape={setShape} setOption={setOption} option={option}/>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
+
+
+export default Home
